8bf047ed445abb519f546beeda45682a17141ba2
Matheus Viscki <matheusviscki@gmail.com>
2023-11-29 21:06:27 -0300

    feat: Adicionado informações faltantes

--- src/main/java/br/com/matheusviscki/gestao_vagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/matheusviscki/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -7,9 +7,14 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
+
 @Entity(name = "company")
+@Data
 public class CompanyEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -28,4 +33,7 @@ public class CompanyEntity {
     private String website;
     private String name;
     private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
