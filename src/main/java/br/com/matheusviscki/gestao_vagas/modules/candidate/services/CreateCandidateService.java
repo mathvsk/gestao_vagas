dd8d8d599a4ebce0dd365f1d38a9c7755c5c2910
Matheus Viscki <matheusviscki@gmail.com>
2023-12-01 20:35:57 -0300

    refactor: Create candidate
    - Implementado criptografia de senha

--- src/main/java/br/com/matheusviscki/gestao_vagas/modules/candidate/services/CreateCandidateService.java
+++ src/main/java/br/com/matheusviscki/gestao_vagas/modules/candidate/services/CreateCandidateService.java
@@ -4,6 +4,7 @@ import br.com.matheusviscki.gestao_vagas.exceptions.UserFoundException;
 import br.com.matheusviscki.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.matheusviscki.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,17 @@ public class CreateCandidateService {
     @Autowired
     CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
             .ifPresent(candidate -> {
                 throw new UserFoundException();
             });
+
+        var password = this.passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return  this.candidateRepository.save(candidateEntity);
 
