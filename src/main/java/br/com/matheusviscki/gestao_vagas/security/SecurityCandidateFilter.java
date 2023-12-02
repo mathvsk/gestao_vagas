429d094e182996b14e3daef78fa134a747b52592
Matheus Viscki <matheusviscki@gmail.com>
2023-12-02 18:51:56 -0300

    feat: Adicionado validação para filtro de segurança
    - Adicionado filtro para o candidate

--- src/main/java/br/com/matheusviscki/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/matheusviscki/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,43 +1,39 @@
 package br.com.matheusviscki.gestao_vagas.security;
 
-import br.com.matheusviscki.gestao_vagas.providers.JWTProvider;
+import br.com.matheusviscki.gestao_vagas.providers.JWTCandidateProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCandidateFilter extends OncePerRequestFilter {
 
     @Autowired
-    private JWTProvider jwtProvider;
+    JWTCandidateProvider jwtCandidateProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
-        if (request.getRequestURI().startsWith("/company")) {
-            if(header != null) {
-                var subjectToken = this.jwtProvider.validateToken(header);
+        if (request.getRequestURI().startsWith("/candidate")) {
+            if (header != null ) {
+                var token = this.jwtCandidateProvider.validateToken(header);
 
-                if(subjectToken.isEmpty()) {
+                if (token == null) {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
                     return;
                 }
 
-                request.setAttribute("company_id", subjectToken);
-
-                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
-
-                SecurityContextHolder.getContext().setAuthentication(auth);
+                request.setAttribute("candidate_id", token.getSubject());
             }
         }
 
