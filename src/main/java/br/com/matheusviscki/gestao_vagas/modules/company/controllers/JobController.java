754c6f70eb95f53524e20517c27b4026f2ef377c
Matheus Viscki <matheusviscki@gmail.com>
2023-12-06 22:13:11 -0300

    feat: Adicionado descrição para rota

--- src/main/java/br/com/matheusviscki/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/matheusviscki/gestao_vagas/modules/company/controllers/JobController.java
@@ -3,6 +3,13 @@ package br.com.matheusviscki.gestao_vagas.modules.company.controllers;
 import br.com.matheusviscki.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.matheusviscki.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.matheusviscki.gestao_vagas.modules.company.services.CreateJobService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +30,14 @@ public class JobController {
 
     @PostMapping("/")
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Informações das vagas")
+    @Operation(summary = "Cadastro de vaga", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(schema = @Schema(implementation = JobEntity.class))
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         var companyID = request.getAttribute("company_id");
         var convertedCompanyID = UUID.fromString(companyID.toString());
