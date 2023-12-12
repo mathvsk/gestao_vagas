12c67b49f03769c1f6f3bdfd60262e91f865fda7
Matheus Viscki <matheusviscki@gmail.com>
2023-12-12 20:41:40 -0300

    fix: SecurityConfig

--- src/main/java/br/com/matheusviscki/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/matheusviscki/gestao_vagas/security/SecurityConfig.java
@@ -15,12 +15,16 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
 
     @Autowired
-    SecurityFilter securityFilter;
+    private SecurityCompanyFilter securityCompanyFilter;
+
+    @Autowired
+    private SecurityCandidateFilter securityCandidateFilter;
 
     private static final String[] SWAGGER_LIST = {
             "/swagger-ui/**",
             "/v3/api-docs/**",
             "/swagger-resource/**",
+            "/actuator/**"
     };
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -30,12 +34,11 @@ public class SecurityConfig {
                     auth.requestMatchers("/company/").permitAll();
                     auth.requestMatchers("/company/auth").permitAll();
                     auth.requestMatchers("/candidate/auth").permitAll();
-                    auth.requestMatchers("/candidate/job").permitAll();
                     auth.requestMatchers(SWAGGER_LIST).permitAll();
 
                     auth.anyRequest().authenticated();
-                }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class)
-                .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+                }).addFilterBefore(this.securityCandidateFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(this.securityCompanyFilter, BasicAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
