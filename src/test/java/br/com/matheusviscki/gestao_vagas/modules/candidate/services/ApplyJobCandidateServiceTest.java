2d4e9b25018a1e88325210948107626812ba93d9
Matheus Viscki <matheusviscki@gmail.com>
2023-12-12 19:09:24 -0300

    test: Aplicar para job

--- src/test/java/br/com/matheusviscki/gestao_vagas/modules/candidate/services/ApplyJobCandidateServiceTest.java
+++ src/test/java/br/com/matheusviscki/gestao_vagas/modules/candidate/services/ApplyJobCandidateServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.matheusviscki.gestao_vagas.modules.candidate.services;
 
+import br.com.matheusviscki.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.matheusviscki.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.matheusviscki.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.matheusviscki.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.matheusviscki.gestao_vagas.modules.company.repositories.JobRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +12,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplyJobCandidateServiceTest {
@@ -31,6 +37,23 @@ public class ApplyJobCandidateServiceTest {
             this.applyJobCandidateService.execute(null, null);
         }catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("Should not be able to apply for a job if job does not found")
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+        var idCandidate = UUID.randomUUID();
+
+        var candidate = new CandidateEntity();
+        candidate.setId(idCandidate);
+
+        when(this.candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+
+        try {
+            this.applyJobCandidateService.execute(idCandidate, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }
