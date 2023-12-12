d0eca4bd4af5f282a98de9640bccac61e9c6d731
Matheus Viscki <matheusviscki@gmail.com>
2023-12-12 19:34:39 -0300

    feat: ApplyJobCandidate - Repository - Interface

---
+++ src/main/java/br/com/matheusviscki/gestao_vagas/modules/candidate/repository/ApplyJobRepository.java
@@ -0,0 +1,9 @@
+package br.com.matheusviscki.gestao_vagas.modules.candidate.repository;
+
+import br.com.matheusviscki.gestao_vagas.modules.candidate.entity.ApplyJobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ApplyJobRepository extends JpaRepository<ApplyJobEntity, UUID> {
+}
