65373da2928d4523408919c8c91c93f3c63e3bf1
Matheus Viscki <matheusviscki@gmail.com>
2025-04-27 15:33:05 -0300

    feat: adicionar validação para parametro da requisição

--- src/main/java/com/matheus/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/matheus/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,13 +1,22 @@
 package com.matheus.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 // Cria getters e setters para todos os atributos
 @Data
 public class CandidateEntity {
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "Username should not be empty and should not contain only spaces")
     private String username;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Length(min = 10, max = 100, message = "Password should be between 10 and 100 characters")
     private String password;
     private String description;
     private String curriculum;
