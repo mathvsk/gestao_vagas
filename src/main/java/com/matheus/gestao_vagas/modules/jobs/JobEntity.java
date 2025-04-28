81e0a9bc8b4ed892fd4ddbfa06de922bd8aabb9d
Matheus Viscki <matheusviscki@gmail.com>
2025-04-28 19:28:31 -0300

    feat: recuperar companyId da requisição e adaptar criação de um JobEntity

--- src/main/java/com/matheus/gestao_vagas/modules/jobs/JobEntity.java
+++ src/main/java/com/matheus/gestao_vagas/modules/jobs/JobEntity.java
@@ -2,7 +2,10 @@ package com.matheus.gestao_vagas.modules.jobs;
 
 import com.matheus.gestao_vagas.modules.company.CompanyEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -10,6 +13,9 @@ import java.util.UUID;
 
 @Data
 @Entity(name = "job")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
--- src/main/java/com/matheus/gestao_vagas/modules/jobs/controllers/JobController.java
+++ src/main/java/com/matheus/gestao_vagas/modules/jobs/controllers/JobController.java
@@ -1,14 +1,15 @@
 package com.matheus.gestao_vagas.modules.jobs.controllers;
 
 import com.matheus.gestao_vagas.modules.jobs.JobEntity;
+import com.matheus.gestao_vagas.modules.jobs.dto.CreateJobDTO;
 import com.matheus.gestao_vagas.modules.jobs.useCases.CreateJobUseCase;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -17,8 +18,17 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<JobEntity> create(@RequestBody JobEntity job) {
-        var result = this.createJobUseCase.execute(job);
+    public ResponseEntity<JobEntity> create(@RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
+        var companyId = request.getAttribute("company_id");
+
+        var jobEntity = JobEntity.builder()
+            .description(createJobDTO.getDescription())
+            .level(createJobDTO.getLevel())
+            .benefits(createJobDTO.getBenefits())
+            .companyId(UUID.fromString(companyId.toString()))
+            .build();
+
+        var result = this.createJobUseCase.execute(jobEntity);
 
         return ResponseEntity.ok(result);
     }
