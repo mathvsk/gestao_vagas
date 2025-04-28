7778454a742fcd8eff8728862e0421a4713ac48a
Matheus Viscki <matheusviscki@gmail.com>
2025-04-28 06:54:31 -0300

    feat: adicionar funcionalidade de codificação da senha salva no db

--- src/main/java/com/matheus/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/com/matheus/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,6 +4,7 @@ import com.matheus.gestao_vagas.exceptions.UserFoundException;
 import com.matheus.gestao_vagas.modules.company.CompanyEntity;
 import com.matheus.gestao_vagas.modules.company.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,12 +13,18 @@ public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CompanyEntity execute(CompanyEntity company) {
         this.companyRepository
             .findByUsernameOrEmail(company.getUsername(), company.getEmail())
             .ifPresent((user) -> {
                 throw new UserFoundException();
             });
+
+        var passwordEncoder = this.passwordEncoder.encode(company.getPassword());
+        company.setPassword(passwordEncoder);
 
         this.companyRepository.save(company);
 
