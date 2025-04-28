bc4bf442929768884d39a76b09d9b57a841e5c79
Matheus Viscki <matheusviscki@gmail.com>
2025-04-28 20:20:40 -0300

    feat: criar AuthCandidateController

---
+++ src/main/java/com/matheus/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -0,0 +1,29 @@
+package com.matheus.gestao_vagas.modules.candidate.controllers;
+
+import com.matheus.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.matheus.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCandidateController {
+
+    @Autowired
+    private AuthCandidateUseCase authCandidateUseCase;
+
+    @PostMapping("/candidate")
+    public ResponseEntity<Object> create(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
+        try {
+            var token = this.authCandidateUseCase.execute(authCandidateRequestDTO);
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
