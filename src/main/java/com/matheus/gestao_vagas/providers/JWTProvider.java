07cc90ad061e6a4ceb5bd8196592a354f46508f1
Matheus Viscki <matheusviscki@gmail.com>
2025-04-28 08:32:12 -0300

    fix: corrigir parametro parassado para funcao de geração de algoritmo

--- src/main/java/com/matheus/gestao_vagas/providers/JWTProvider.java
+++ src/main/java/com/matheus/gestao_vagas/providers/JWTProvider.java
@@ -3,14 +3,18 @@ package com.matheus.gestao_vagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JWTProvider {
+    @Value("${jwt.key}")
+    private String secretKey;
+
     public String validateToken(String token) {
         token = token.replace("Bearer ", "");
 
-        Algorithm algorithm = Algorithm.HMAC256(token);
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
         try {
             return JWT.require(algorithm)
