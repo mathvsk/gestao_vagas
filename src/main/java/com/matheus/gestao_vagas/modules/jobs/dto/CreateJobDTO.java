0ffde33f32490d9f8f00b1476a79b2dc9f37dfd7
Matheus Viscki <matheusviscki@gmail.com>
2025-05-07 07:52:10 -0300

    feat: inserir documentacao mais completa na rota de criar uma vaga, com exemplos reais de body request

--- src/main/java/com/matheus/gestao_vagas/modules/jobs/dto/CreateJobDTO.java
+++ src/main/java/com/matheus/gestao_vagas/modules/jobs/dto/CreateJobDTO.java
@@ -1,10 +1,16 @@
 package com.matheus.gestao_vagas.modules.jobs.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+    @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = Schema.RequiredMode.REQUIRED)
     private String description;
+
+    @Schema(example = "GymPass, Plano de saúde", requiredMode = Schema.RequiredMode.REQUIRED)
     private String benefits;
+
+    @Schema(example = "JUNIOR", requiredMode = Schema.RequiredMode.REQUIRED)
     private String level;
 }
