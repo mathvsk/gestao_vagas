7202ff5204bff4848ee94f817dd19ffeff132305
Matheus Viscki <matheusviscki@gmail.com>
2025-05-07 08:19:25 -0300

    feat: documentar rota de autenticação do usuario

--- src/main/java/com/matheus/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
+++ src/main/java/com/matheus/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -1,7 +1,14 @@
 package com.matheus.gestao_vagas.modules.candidate.controllers;
 
 import com.matheus.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import com.matheus.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
 import com.matheus.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "rotas do candidato")
 public class AuthCandidateController {
 
     @Autowired
     private AuthCandidateUseCase authCandidateUseCase;
 
     @PostMapping("/auth")
+    @Operation(summary = "Autenticação de candidato", description = "Essa função é responsável por autenticar um candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = AuthCandidateResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "Usuário ou senha inválidos")
+    })
     public ResponseEntity<Object> create(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
         try {
             var token = this.authCandidateUseCase.execute(authCandidateRequestDTO);
